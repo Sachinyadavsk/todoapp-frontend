065376dcd5e17412c5b626f0dafa0fb5b6d53588
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-07-13 14:35:56 +0530

    add local and live url

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,5 @@
 
-const api = "http://localhost:8000/api";
+const api = "https://todoapp-api-jlyt.onrender.com/api";
+// https://todoapp-api-jlyt.onrender.com
+// http://localhost:8000/api
 export default api;
